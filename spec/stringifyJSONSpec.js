51436b8db15c322e1b694b9c1e69d29ae7e46971
sdtsui <danielt213@gmail.com>
2014-11-18 12:16:44 -0800

    stringifyJSON done

--- spec/stringifyJSONSpec.js
+++ spec/stringifyJSONSpec.js
@@ -1,13 +1,14 @@
 // test cases are described in fixtures.js
 describe('stringifyJSON', function(){
   it('should match the result of calling JSON.stringify', function(){
-
+  	//alert('stringifiable:');
+	//alert(stringifiableObjects);
     stringifiableObjects.forEach(function(test){
       var result = stringifyJSON(test);
       var expected = JSON.stringify(test);
       expect(result).to.equal(expected);
     });
-
+	//alert(unstringifiableValues);
     unstringifiableValues.forEach(function(obj){
       var result = stringifyJSON(obj);
       var expected = JSON.stringify(obj);
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,7 +1,69 @@
+//You can use recursion on problems where smaller parts of the problem look the same as the larger problem as a whole.
+
+/*
+You always want to think of 3 things when you write recursive function: Base Case, Step, and Stop Condition. 
+There's always a base case you need to provide so that recursion can be built up. 
+Step defines how the recursion is getting reduced. 
+Stop condition is a true-or-false statement that tells recursion when to stop. 
+If you go with these 3 elements you should have a better time solving problem.
+*/
+
+
 // this is what you would do if you liked things to be easy:
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+	var JSON = '';
+	if (obj === null){
+		return 'null';
+    }else if(obj === undefined){
+    	return 'undefined';
+    }else if (typeof(obj)==='string'){
+      JSON += '\"' +obj.toString()+'\"';
+      return JSON;
+    }else if (typeof(obj)=== 'number' || typeof(obj)=== 'boolean'){
+		JSON += obj.toString();
+		return JSON;
+	}else if (Object.prototype.toString.call( obj ) === '[object Array]'){
+      	JSON += '[';
+		//alert('array');
+      	//Base: array of some length, call stringify on first element
+		//step: return a recursive call to array.slice(index+1);
+		//stop: array of length zero, return '', let higher level calls return full string
+      for (var i=0; i<obj.length; i++){
+        JSON += stringifyJSON(obj[i]);
+        if (i !== obj.length-1){
+          JSON += ',';
+        }
+      }
+      //could also use _.each(obj,stringifyJSON)
+      JSON += ']';
+      return JSON;
+		
+		
+	}else if(Object.prototype.toString.call(obj )==='[object Object]'){
+      JSON += '{'
+		//alert('object');
+      //Base: object with some properties, call stringify on all elements
+      //step: 
+      //stop: 
+      
+      for (var prop in obj){
+      	if(typeof(obj[prop]) === 'function'){      		
+      	}else if ((obj[prop] !== undefined)){
+      		//console.log(typeof(obj[prop]));
+      		//alert(prop.toString() + ':'+obj[prop]);
+      		JSON += '\"'+prop.toString() + '\":' + stringifyJSON(obj[prop])+',';
+      	}
+      }
+      if (JSON.length > 1){
+      	JSON = JSON.slice(0,-1);
+      }
+      JSON += '}';
+      return JSON;
+	}
+	
+  
+  
 };
